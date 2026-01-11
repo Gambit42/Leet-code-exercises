3ed62e6b78ec2f4a6d93ce5aff8d4018354724ec
Prins Ocampo <85613393+Gambit42@users.noreply.github.com>
2026-01-12 00:15:16 +0800

    chore: refactor merge alternately

--- ArrayString/MergeStringsAlternately.ts
+++ ArrayString/MergeStringsAlternately.ts
@@ -2,86 +2,24 @@
 
 // Return the merged string.
 
-const handleConvertToArray = (word: string) => {
-  return [...word];
-};
+const mergeAlternately = (word1: string, word2: string): string => {
+  let merged: string[] = [];
+  let lastIndexUsed = 0;
 
-const handleMergeStringsAlternately = (word1: string, word2: string) => {
-  if (1 > word1.length || word2.length > 100) return;
-  let mergedArray = [] as string[];
-  const WORD_1_ARRAY = handleConvertToArray(word1);
-  const WORD_2_ARRAY = handleConvertToArray(word2);
-
-  //If the first word is longer than the second word, append the remaining letters not able to be alternated
-  if (word1.length > word2.length) {
-    WORD_1_ARRAY.map((firstWordElement, index) => {
-      mergedArray = [...mergedArray, firstWordElement];
-      if (WORD_2_ARRAY[index] === undefined) return;
-      mergedArray = [...mergedArray, WORD_2_ARRAY[index]];
-    });
-    console.log(mergedArray.join(""));
-    return mergedArray.join("");
-  }
-
-  //If the second word is longer than the first word, append the remaining letters not able to be alternated
-  if (word1.length < word2.length) {
-    WORD_1_ARRAY.map((firstWordElement, index) => {
-      mergedArray = [...mergedArray, firstWordElement];
-      if (WORD_2_ARRAY[index] === undefined) return;
-      mergedArray = [...mergedArray, WORD_2_ARRAY[index]];
-    });
-
-    mergedArray = [
-      ...mergedArray,
-      ...WORD_2_ARRAY.slice(WORD_1_ARRAY.length - WORD_2_ARRAY.length),
-    ];
-    console.log(mergedArray.join(""));
-    return mergedArray.join("");
-  }
-
-  WORD_1_ARRAY.map((firstWordElement, index) => {
-    mergedArray = [...mergedArray, firstWordElement, WORD_2_ARRAY[index]];
+  [...word1].map((letter, index) => {
+    merged.push(letter);
+    lastIndexUsed = index;
+    if (!word2[index]) return;
+    merged.push(word2[index]);
   });
 
-  console.log(mergedArray.join(""));
+  if (word1.length < word2.length) {
+    return [...merged, ...word2.slice(lastIndexUsed + 1)].join("");
+  }
 
-  return mergedArray.join("");
+  return merged.join("");
 };
 
-// Example 1:
-
-// Input: word1 = "abc", word2 = "pqr"
-// Output: "apbqcr"
-// Explanation: The merged string will be merged as so:
-// word1:  a   b   c
-// word2:    p   q   r
-// merged: a p b q c r
-
-handleMergeStringsAlternately("abc", "pqr");
-
-// Example 2:
-
-// Input: word1 = "ab", word2 = "pqrs"
-// Output: "apbqrs"
-// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
-// word1:  a   b
-// word2:    p   q   r   s
-// merged: a p b q   r   s
-
-handleMergeStringsAlternately("ab", "pqrd");
-
-// Example 3:
-
-// Input: word1 = "abcd", word2 = "pq"
-// Output: "apbqcd"
-// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
-// word1:  a   b   c   d
-// word2:    p   q
-// merged: a p b q c   d
-
-handleMergeStringsAlternately("abcd", "pq");
-
-// Constraints:
-
-// 1 <= word1.length, word2.length <= 100
-// word1 and word2 consist of lowercase English letters.
+console.log(mergeAlternately("abc", "pqr"));
+console.log(mergeAlternately("ab", "pqrs"));
+console.log(mergeAlternately("abcd", "pq"));
